92b0cbe3be5138e2cd5f20a948c16ffd76cc48e5
Jamie Yu <jme.kim.yu@gmail.com>
2017-03-01 11:47:26 -0500

    homepage blurb and styling

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,9 +4,20 @@ import { Link } from 'react-router';
 
 export const Home = () => (
   <div>
-    <img className="float-left" src="aboutPuppy.png" />
+    <img className="home-page-image" src="aboutPuppy.png" />
     <h1>Welcome To VYNL</h1>
-    <p>We sell records.</p>
+    <p>
+      Vinyl. An experience that encloses you in its warm, organic soundscape. Surrender yourself to the delights of analog
+      sound and forsake the fool’s gold standard of digital audio.
+    </p>
+    <p>
+      VYNL offers a hand-curated, truly esoteric selection of artists on the format they were intended to be appreciated -
+      the vinyl record. Allow us to be your personal sommelier of sound. Trust us to deliver you the best in music before it
+      becomes tainted by the Mainstream.
+    </p>
+    <p>
+      Don’t hesitate. VYNL.
+    </p>
   </div>
 );
 
