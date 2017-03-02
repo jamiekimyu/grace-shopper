eb52cc7cedc30db6fe55882946a7510fb455db97
Jamie Yu <jme.kim.yu@gmail.com>
2017-03-02 18:08:25 -0500

    fixed bug, image renders properly

--- app/components/Record.jsx
+++ app/components/Record.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-export const Record = (props) => (
+export const Record = ({record, product}) => (
   <div className="container-fluid">
-	{/*JSON.stringify(props.record)*/}
     <h4>Record</h4>
-		{/*<img src={props.record.product.photo} className="img-thumbnail" />*/}
-		<h1>{props.record.artist}</h1>
-		<h1>{props.record.title}</h1>
-		<h1>{props.record.product}</h1>
+		<img src={record.product && record.product.photo} className="img-thumbnail" />
+		<h1>Artist: {record.artist}</h1>
+		<h1>Album: {record.title}</h1>
   </div>
 
 );
 
-const mapStateToProps = ({record}) => ({record});
+const mapStateToProps = ({record}) => ({
+	record,
+	//product: record.product || {}
+});
 
 const mapDispatchToProps = () => ({});
 
