bedd833fdaf9a7a72ef6985aa70bff8b90bbd3ec
Kim Winston-Jackson <klwjack@gmail.com>
2017-03-02 11:55:01 -0600

    have admin-ui display mostly working

--- app/components/Admin/index.jsx
+++ app/components/Admin/index.jsx
@@ -3,64 +3,13 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import ProductsTable from './ProductsTable';
 import {deleteProduct} from '../../reducers/products';
-
+import Form from './Form';
 export const Admin = ({products, current, deleteProduct}) => (
   <div>
     {JSON.stringify(current)}
     <ProductsTable products={products} deleteProduct={deleteProduct} />
     <hr />
-    <form>
-      <div className="form-group">
-        <label htmlFor="admin-type">Type</label>
-        <select className="form-control" id="admin-type">
-          <option>Service</option>
-          <option>Record</option>
-          <option>Equipment</option>
-        </select>
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="admin-name">Name</label>
-        <input type="text" className="form-control" id="admin-name" />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="admin-price">Price</label>
-        <input type="number" min="0" className="form-control" id="admin-price" />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="admin-description">Description</label>
-        <textbox className="form-control" id="admin-description" />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="admin-photo">Photo URL</label>
-        <input type="text" className="form-control" id="admin-photo" />
-      </div>
-
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          Record Information
-        </div>
-        <div className="panel-body">
-          <div className="form-group">
-            <label htmlFor="admin-record-artist">Artist</label>
-            <input type="text" className="form-control" id="admin-record-artist" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="admin-record-release-date">Release Date</label>
-            <input type="date" className="form-control" id="admin-record-release-date" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="admin-record-genre">Genre</label>
-            <input type="text" className="form-control" id="admin-record-genre" />
-          </div>
-        </div>
-      </div>
-    </form>
+    <Form current={current} />
   </div>
 );
 
