45398be843d5da120182c54346f38d246b7d296f
Jason PowellE <jpdevelopment@icloud.com>
2017-03-03 12:01:53 -0500

    working on login

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 export const Login = ({ login }) => (
   <form onSubmit={evt => {
+	console.log(document.activeElement.id);
 	evt.preventDefault();
-	login(evt.target.username.value, evt.target.password.value);
+	if (document.activeElement.id === 'register'){
+		// reducer
+	} else {
+		login(evt.target.username.value, evt.target.password.value);
+	}
 } }>
-    <input name="username" />
-    <input name="password" type="password" />
-    <input type="submit" value="Login" />
+    <input name="username" placeholder="Email" className="form-control" />
+    <input name="password" type="password" placeholder="Password" className="form-control" />
+    <input type="submit" id="login" value="Login" className="btn btn-primary" />
+		<input type="submit" id="register" value="Register" className="btn btn-primary" />
   </form>
 );
 
