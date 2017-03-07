77459c6513bf741249c05f5a2163a29a982f4ccb
Aaron Aichlmayr <waterfoul@gmail.com>
2017-03-06 21:31:01 -0600

    Personal code review

--- server/api.js
+++ server/api.js
@@ -4,10 +4,10 @@ const db = require('APP/db');
 const api = module.exports = require('express').Router();
 
 api
-  .get('/heartbeat', (req, res) => res.send({ok: true}))
-  .use('/auth', require('./auth'))
-  .use('/users', require('./users'))
-  .use('/products', require('./products'))
+	.get('/heartbeat', (req, res) => res.send({ok: true}))
+	.use('/auth', require('./auth'))
+	.use('/users', require('./users'))
+	.use('/products', require('./products'))
 	.use('/records', require('./records'))
 	.use('/orders', require('./orders'));
 
