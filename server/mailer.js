b59a37e4db30e5c4895a034b3311370d542a0eee
Aaron Aichlmayr <waterfoul@gmail.com>
2017-03-07 10:59:19 -0600

    Cleaned up logging

--- server/mailer.js
+++ server/mailer.js
@@ -6,7 +6,6 @@ const sender = sendgrid(env.SENDGRID_API_KEY);
 
 module.exports = {
 	sendMail: ({to, subject, html}) => {
-		console.log({to, subject, html});
 		const from = new sendgrid.mail.Email('VYNL <waterfoul+vynl@gmail.com>');
 		to = new sendgrid.mail.Email(to);
 		html = new sendgrid.mail.Content('text/html', html);
@@ -20,7 +19,6 @@ module.exports = {
 
 		return new Promise((resolve, reject) => {
 			sender.API(request, function(error, response) {
-				console.log(response);
 				if(error) reject(error);
 				else resolve(response);
 			});
