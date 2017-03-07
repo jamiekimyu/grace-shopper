f9ba757d974a94ebdd0419d398a21658321dbf73
Aaron Aichlmayr <waterfoul@gmail.com>
2017-03-07 15:27:45 -0600

    adding some fields

--- app/components/User/Settings.jsx
+++ app/components/User/Settings.jsx
@@ -2,13 +2,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-export const Settings = () => (
+export const Settings = ({auth}) => (
   <div>
-    Coming Soon Services&trade;
+    <div className="form-group col-md-12">
+      <label htmlFor="user-name">Name</label>
+      <input
+        type="text"
+        className="form-control"
+        id="user-name"
+        value={auth && auth.name || ''}
+        onChange={() => {}}
+      />
+    </div>
+    <div className="form-group col-md-12">
+      <label htmlFor="user-email">Email</label>
+      <input
+        type="text"
+        className="form-control"
+        id="user-email"
+        value={auth && auth.email || ''}
+        onChange={() => {}}
+      />
+    </div>
+    <div className="form-group col-md-12">
+      <label htmlFor="user-password">Password</label>
+      <input
+        type="text"
+        className="form-control"
+        id="user-password"
+        value={auth && auth.password || ''}
+        onChange={() => {}}
+      />
+    </div>
   </div>
 );
 
-const mapStateToProps = () => ({});
+const mapStateToProps = ({auth}) => ({auth});
 
 const mapDispatchToProps = () => ({});
 
