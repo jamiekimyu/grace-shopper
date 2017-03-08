9e1d7890bbccd99286c1a22afbd1b57ce3d5f9cf
Kim Winston-Jackson <klwjack@gmail.com>
2017-03-07 22:57:12 -0600

    review-ui working

--- app/components/Review.jsx
+++ app/components/Review.jsx
@@ -7,10 +7,35 @@ import { Link } from 'react-router';
 export const Review = ({product}) => (
 	<div>
 		<h2>Reviews</h2>
-		<Link className="btn btn-primary style-review-button" to={`/ReviewForm/${product && product.id}`}>Add a Review!</Link>
 		<div className="panel panel-default">
-			<div className="panel-body">{JSON.stringify(product && product.reviews)}</div>
+			<div className="panel-body">
+				<ol>
+					{
+						product && product.reviews.map((review) => (
+							<li key={review.id} className="container-fluid">
+								<div>
+									Review: {review.comment}
+								</div>
+								<div>
+									Rating: {review.rating}
+								</div>
+							</li>
+						))
+					}
+				</ol>
+			</div>
 		</div>
+		<Link className="btn btn-primary style-review-button" to={`/ReviewForm/${product && product.id}`}>Add a Review!</Link>
 	</div>
 
 );
+{/*
+	JSON.stringify(product && product.reviews)
+	product.reviews.map((review) => (
+		<li key={review.id} className="container-fluid">
+			<div>
+				{review.comment}
+			</div>
+		</li>
+	))
+*/}
